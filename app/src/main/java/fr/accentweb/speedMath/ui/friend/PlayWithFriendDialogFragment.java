0a126d540acabaa4da302a95960ca7828ae30368
danielp67 <daniel.67100@hotmail.fr>
2026-02-10 22:38:37 +0100

    sync online data in PlayWithFriendDialogFragment when opening dialog

--- app/src/main/java/fr/accentweb/speedMath/ui/friend/PlayWithFriendDialogFragment.java
+++ app/src/main/java/fr/accentweb/speedMath/ui/friend/PlayWithFriendDialogFragment.java
@@ -9,14 +9,32 @@ import androidx.fragment.app.DialogFragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import fr.accentweb.speedMath.R;
+import fr.accentweb.speedMath.core.PlayerManager;
 
 public class PlayWithFriendDialogFragment extends DialogFragment {
+
+    private PlayerManager playerManager;
+    private String uid, pseudo;
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         View v = requireActivity().getLayoutInflater().inflate(R.layout.dialog_play_with_friend, null);
 
+        playerManager = PlayerManager.getInstance(requireContext());
+
+        uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        pseudo = playerManager.getOnlinePseudo();
+
+        if(!playerManager.getTodayDate().equals(playerManager.getLastConnection()))
+        {
+            DatabaseReference playerRef = FirebaseDatabase.getInstance().getReference("players").child(uid);
+            playerManager.syncOnlineData(playerRef);
+        }
         AlertDialog dialog = new androidx.appcompat.app.AlertDialog.Builder(requireContext(), R.style.SpeedMath_Dialog)
                 .setView(v)
                 .create();
