b91bd4f400c6dfd779e8cdb39a74a0e93e0aac5f
danielp67 <daniel.67100@hotmail.fr>
2026-07-27 23:09:15 +0200

    downgrade androidx.core:core-ktx from 1.19.0 to 1.17.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,7 +56,7 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:34.16.0"))
     implementation("com.google.firebase:firebase-analytics")
     implementation("com.google.firebase:firebase-database")
-    implementation("androidx.core:core-ktx:1.19.0")
+    implementation("androidx.core:core-ktx:1.17.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
     implementation("com.google.firebase:firebase-inappmessaging-display")
     implementation("com.google.firebase:firebase-analytics")
